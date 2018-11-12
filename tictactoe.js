117fe819a400f7bca67de73178642d4bbb9a1efd
Danny Liehr <dannyisyelling@gmail.com>
2018-11-12 14:15:37 -0500

    Hopefully the js goes through

--- tictactoe.js
+++ tictactoe.js
@@ -13,6 +13,7 @@
 // •	Make sure it looks good
 ///first ma
 const circleSymbol = "⭕️";
+//FUCK
 const crossSymbol = "❌";
 const winCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
 const gameBoard = document.getElementById('board');
