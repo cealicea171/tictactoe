ad1fd16cdae85d564fa69b7126719e8186873aab
Calista Alicea <cealicea171@stevenscollege.edu>
2018-11-12 13:04:51 -0500

    js rules and css files

---
+++ tictactoe.js
@@ -0,0 +1,14 @@
+//•   first start 3X3 GRID
+// make a class for each cell
+//•	2 players
+//•	Only one winner
+//•	Place one symbol per turn, one empty space
+//•	Players take turns
+//•	Three in a row wins up and down
+// topright to bottomleft
+// left and right topleft to bottomright
+// •	If full, no winner, restart till death
+// •	Randomly choose who goes first
+// •	Place symbol by clicking a square or with a keyboard
+// •	Make sure it looks good
+///first ma
